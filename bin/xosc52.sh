456f5d5e404cca21cba06aff7ad6b14a642888ec
Sungkwang Lee <sungkwanglee@google.com>
2023-06-19 21:10:38 +0100

    SSH_TTY sometimes doesn't work well as intended

--- bin/xosc52.sh
+++ bin/xosc52.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-if [[ -z "$SSH_TTY" ]]; then
-  _pid=$$
-  while ! readlink "/proc/$_pid/fd/0" | grep -E "pts|tty" > /dev/null; do
-    _pid=$(cat "/proc/$_pid/stat" | awk '{ print $4 }')
-    [[ "$_pid" == "0" ]] && break
-  done
-  _tty=$(readlink "/proc/$_pid/fd/0")
-else
-  _tty=$SSH_TTY
-fi
+_pid=$$
+while ! readlink "/proc/$_pid/fd/0" | grep -E "pts|tty" > /dev/null; do
+  _pid=$(cat "/proc/$_pid/stat" | awk '{ print $4 }')
+  [[ "$_pid" == "0" ]] && break
+done
+_tty=$(readlink "/proc/$_pid/fd/0")
 
 osc52.sh $@ > $_tty
