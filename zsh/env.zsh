42054a6cca9803b70f734fb26f3493c22a78e2e5
Sungkwang Lee <sungkwanglee@google.com>
2023-06-19 21:11:42 +0100

    Add corp/bin path

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -18,7 +18,7 @@ export LC_CTYPE=en_US.UTF-8
 export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 export EDITOR='nvim'
-export PATH=/usr/local/go/bin:$HOME/bin:$PATH:/snap/bin:$HOME/.cargo/bin:$HOME/.yarn/bin:$HOME/.local/bin:$HOME/go/bin:$_DOTFILESDIR/bin
+export PATH=/usr/local/go/bin:$HOME/bin:$PATH:/snap/bin:$HOME/.cargo/bin:$HOME/.yarn/bin:$HOME/.local/bin:$HOME/go/bin:$_DOTFILESDIR/bin:$_DOTFILESDIR/corp/bin
 
 if ls --color -d . >/dev/null 2>&1; then
     # GNU ls
