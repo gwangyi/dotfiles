4a5de5e445702e8fee5612827c15cb114c8673cf
Sungkwang Lee <gwangyi.kr@gmail.com>
2025-06-16 15:09:57 +0900

    make p10k instant prompt to the next of zpm.

--- zsh/init.zsh
+++ zsh/init.zsh
@@ -1,12 +1,3 @@
-# {{{ Powerlevel10k instant prompt
-# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
-# Initialization code that may require console input (password prompts, [y/n]
-# confirmations, etc.) must go above this block; everything else may go below.
-if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
-  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
-fi
-# }}}
-
 # {{{ Default .zshrc contents
 # Set up the prompt
 
@@ -95,6 +86,15 @@ function __install_tools {
 }
 
 [ "$(cat ${_dotfiles_dir}/.tools 2>/dev/null)" = "${_tool_ver}" ] || __install_tools
+# }}}
+
+# {{{ Powerlevel10k instant prompt
+# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+# Initialization code that may require console input (password prompts, [y/n]
+# confirmations, etc.) must go above this block; everything else may go below.
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
 # }}}
 
 # {{{ ZPM
