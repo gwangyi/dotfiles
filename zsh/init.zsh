ad8f67b72f51de2094a9ec799bc0debebb12c43d
Sungkwang Lee <gwangyi.kr@gmail.com>
2025-07-17 10:01:28 +0900

    Add more useful tools

--- zsh/init.zsh
+++ zsh/init.zsh
@@ -121,7 +121,9 @@ zpm load @dir/nvim-dotfiles,origin:"${_dotfiles_dir}/nvim",apply:source,hook:"./
 
 zpm load junegunn/fzf,hook:"./install --bin && cat shell/*.zsh > init.zsh",async
 zpm load @empty/ripgrep,hook:"cargo binstall -y ripgrep",async
-zpm load @exec/lsd,origin:"echo alias ls=lsd",hook:"cargo binstall -y lsd",async
+zpm load @exec/eza,origin:"echo alias ls=eza",hook:"cargo binstall -y eza",async
+zpm load @empty/bat,hook:"cargo binstall -y bat",async
+zpm load @empty/gemini,hook:"pnpm i -g @google/gemini-cli",async
 
 zpm load @empty/compiledb,hook:"go install github.com/fcying/compiledb-go/cmd/compiledb@latest",async
 
