6ccdcdd0963f8b59990a7d4eb6b71ca42a6a1a74
Sungkwang Lee <gwangyi.kr@gmail.com>
2026-04-22 12:56:45 +0900

    nvim: nvim-goc for go coverage

--- nvim/lua/plugins/go.lua
+++ nvim/lua/plugins/go.lua
@@ -27,5 +27,16 @@ return {
     event = {"CmdlineEnter"},
     ft = {"go", 'gomod'},
     build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
+  },
+  {
+    "rafaelsq/nvim-goc.lua",
+    opts = {},
+    config = function(lp, opts)
+      local goc = require('nvim-goc')
+      goc.setup(opts)
+      vim.keymap.set('n', '<Leader>gcf', goc.Coverage, {silent=true})       -- run for the whole File
+      vim.keymap.set('n', '<Leader>gct', goc.CoverageFunc, {silent=true})   -- run only for a specific Test unit
+      vim.keymap.set('n', '<Leader>gcc', goc.ClearCoverage, {silent=true})  -- clear coverage highlights
+    end
   }
 }
