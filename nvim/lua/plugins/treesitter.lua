d7ea6ab0fbf4d80ad31dfd71fde47c77c3ffba13
Sungkwang Lee <gwangyi.kr@gmail.com>
2026-09-27 00:49:49 +0900

    modernize treesitter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,20 +1,31 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
+    branch = "main",
     build = ":TSUpdate",
     config = function ()
-      require'nvim-treesitter.configs'.setup {
-        -- Modules and its options go here
+      require("nvim-treesitter").setup({
+        -- auto_install is supported directly here
         auto_install = true,
-        highlight = { enable = true },
-        incremental_selection = { enable = true },
-        textobjects = { enable = true },
-        indent = { enable = true },
-      }
+      })
+      vim.api.nvim_create_autocmd("FileType", {
+        callback = function()
+          pcall(vim.treesitter.start)
+        end,
+      })
       if vim.treesitter.highlighter.hl_map ~= nil then
         vim.treesitter.highlighter.hl_map.error = nil
       end
     end
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    branch = "main",
+    config = function()
+      require("nvim-treesitter-textobjects").setup({
+        -- configure textobject keymaps here
+      })
+    end,
   }
 }
 
